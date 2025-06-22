d3b0f5e35f3f806bcc87313c388386582d311da0
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-06-22 20:32:39 +0900

    Feat: Init

---
+++ Source/1.6/HarmonyPatches/HarmonyPatches.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace Maux36.RimPsyche.Disposition
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            var harmony = new Harmony("rimworld.mod.Maux.RimPsyche.Disposition");
+            harmony.PatchAllUncategorized(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Source/1.6/HarmonyPatches/Thought_MoodOffset.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Maux36.RimPsyche.Disposition
+{
+    [HarmonyPatch(typeof(Thought), "MoodOffset")]
+    public static class Thought_MoodOffset
+    {
+        static void Postfix(ref float __result, Pawn ___pawn)
+        {
+            if (___pawn?.compPsyche() is not { } compPsyche || __result == 0f)
+                return;
+            float optimism = compPsyche.Personality.GetPersonality(PersonalityDefOf.Rimpsyche_Optimism); // -1 ~ 1
+            if (__result < 0f)
+            {
+                __result *= (1f - optimism * 0.45f);
+            }
+            else
+            {
+                __result *= (1f + optimism * 0.45f);
+            }
+        }
+    }
+}
