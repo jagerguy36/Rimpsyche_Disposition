4ed89be28391948cf287bea1670a905da0675fa4
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-07-01 01:10:09 +0900

    Feat: implementations

---
+++ Source/1.6/HarmonyPatches/NeedSeeker_NeedInterval.cs
@@ -0,0 +1,86 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Verse;
+
+namespace Maux36.RimPsyche.Disposition
+{
+    public class NeedSeeker_NeedInterval
+    {
+        [HarmonyPatch(typeof(Need_Seeker), nameof(Need_Seeker.NeedInterval))]
+        public static class Patch_NeedSeeker_NeedInterval
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+
+                var pawnField = AccessTools.Field(typeof(Need), "pawn");
+                var getMultiplierMethod = AccessTools.Method(typeof(Patch_NeedSeeker_NeedInterval), nameof(GetMultiplier));
+
+                for (int i = 0; i < codes.Count - 2; i++)
+                {
+                    // seekerRisePerHour * 0.06
+                    if (codes[i].opcode == OpCodes.Ldfld && codes[i].operand.ToString().Contains("seekerRisePerHour") &&
+                        codes[i + 1].opcode == OpCodes.Ldc_R4 && (float)codes[i + 1].operand == 0.06f)
+                    {
+                        codes.Insert(i + 2, new CodeInstruction(OpCodes.Ldarg_0));               // this
+                        codes.Insert(i + 3, new CodeInstruction(OpCodes.Ldfld, pawnField));      // this.pawn
+                        codes.Insert(i + 4, new CodeInstruction(OpCodes.Ldc_I4_1));              // true = rising
+                        codes.Insert(i + 5, new CodeInstruction(OpCodes.Call, getMultiplierMethod)); // call method
+                        codes.Insert(i + 6, new CodeInstruction(OpCodes.Mul));
+                        i += 6;
+                    }
+
+                    // seekerFallPerHour * 0.06
+                    if (codes[i].opcode == OpCodes.Ldfld && codes[i].operand.ToString().Contains("seekerFallPerHour") &&
+                        codes[i + 1].opcode == OpCodes.Ldc_R4 && (float)codes[i + 1].operand == 0.06f)
+                    {
+                        codes.Insert(i + 2, new CodeInstruction(OpCodes.Ldarg_0));
+                        codes.Insert(i + 3, new CodeInstruction(OpCodes.Ldfld, pawnField));
+                        codes.Insert(i + 4, new CodeInstruction(OpCodes.Ldc_I4_0)); // false = falling
+                        codes.Insert(i + 5, new CodeInstruction(OpCodes.Call, getMultiplierMethod));
+                        codes.Insert(i + 6, new CodeInstruction(OpCodes.Mul));
+                        i += 6;
+                    }
+                }
+
+                return codes;
+            }
+
+            // Exposed method: takes pawn directly
+            public static float GetMultiplier(Pawn pawn, bool isRising)
+            {
+                if (pawn == null)
+                    return 1f;
+
+                var psyche = pawn.compPsyche();
+                if (psyche == null)
+                    return 1f;
+
+                return isRising ? psyche.Personality.GetMultiplier(MoodRisingSpeedMultiplier) : psyche.Personality.GetMultiplier(MoodFallingSpeedMultiplier);
+            }
+
+
+            public static RimpsycheMultiplier MoodRisingSpeedMultiplier = new(
+                "MoodRisingSpeedMultiplier",
+                (tracker) =>
+                {
+                    float mult = 1f;
+                    float volatility = tracker.GetPersonalityAsMult(PersonalityDefOf.Rimpsyche_Volatility, 1.5f);
+                    return mult * volatility;
+                }
+            );
+
+            public static RimpsycheMultiplier MoodFallingSpeedMultiplier = new(
+                "MoodFallingSpeedMultiplier",
+                (tracker) =>
+                {
+                    float mult = 1f;
+                    float volatility = tracker.GetPersonalityAsMult(PersonalityDefOf.Rimpsyche_Volatility, 3f);
+                    return mult * volatility;
+                }
+            );
+        }
+    }
+}
---
+++ Source/1.6/StatParts/RimpsycheStatPart.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Maux36.RimPsyche.Disposition
+{
+    public class PsycheVolatilityMentalBreakThreasholdOffset : StatPart// M 0.85 ~ 1.15
+    {
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (req.HasThing && req.Thing is Pawn pawn)
+            {
+                var compPsyche = pawn.compPsyche();
+                if (compPsyche != null)
+                {
+                    val += VolatilityMentalBreakThresholdOffset(compPsyche);
+                }
+            }
+        }
+
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (req.HasThing && req.Thing is Pawn pawn)
+            {
+                var compPsyche = pawn.compPsyche();
+                if (compPsyche != null)
+                {
+                    return "RP_Stat_MentalBreakThreashold".Translate() + VolatilityMentalBreakThresholdOffset(compPsyche).ToStringPercent();
+                }
+            }
+            return null;
+        }
+
+        private float VolatilityMentalBreakThresholdOffset(CompPsyche compPsyche)
+        {
+            return compPsyche.Personality.GetPersonality(PersonalityDefOf.Rimpsyche_Volatility)*0.2f;
+        }
+    }
+}
