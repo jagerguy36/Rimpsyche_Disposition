e3084a94ae39316c63fbb785525e530e2d22cb80
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-07-16 13:42:44 +0900

    Feat: Morality work

--- Source/1.6/HarmonyPatches/Thought_MoodOffset.cs
+++ Source/1.6/HarmonyPatches/Thought_MoodOffset.cs
@@ -7,7 +7,7 @@ namespace Maux36.RimPsyche.Disposition
     [HarmonyPatch(typeof(Thought), "MoodOffset")]
     public static class Thought_MoodOffset
     {
-        static void Postfix(ref float __result, Pawn ___pawn)
+        static void Postfix(ref float __result, Pawn ___pawn, ThoughtDef ___def)
         {
             if (___pawn?.compPsyche() is not { } compPsyche || __result == 0f)
                 return;
@@ -18,6 +18,10 @@ namespace Maux36.RimPsyche.Disposition
             else
             {
                 __result *= compPsyche.Personality.Evaluate(PositiveMoodOffsetMultiplier);
+            }
+            if(___def.sourcePrecept != null)
+            {
+                __result *= compPsyche.Personality.Evaluate(MoralityMoodOffsetMultiplier);
             }
         }
 
@@ -43,5 +47,17 @@ namespace Maux36.RimPsyche.Disposition
                 return mult * optimismMult * emotionalityMult;
             }
         );
+
+        public static RimpsycheFormula MoralityMoodOffsetMultiplier = new(
+            "MoralityMoodOffsetMultiplier",
+            (tracker) =>
+            {
+                float mult = 1f;
+                float moralityMult = 1f + tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Morality) * 0.45f;
+                return mult * moralityMult;
+            }
+        );
+
+        
     }
 }
---
+++ Source/1.6/HarmonyPatches/Thought_SituationalSocial_OpinionOffset.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Maux36.RimPsyche.Disposition
+{
+    [HarmonyPatch(typeof(Thought_SituationalSocial), "OpinionOffset")]
+    public static class Thought_SituationalSocial_OpinionOffset
+    {
+        static void Postfix(ref float __result, Pawn ___pawn, ThoughtDef ___def)
+        {
+            if (___pawn?.compPsyche() is not { } compPsyche || __result == 0f)
+                return;
+            if(___def.sourcePrecept != null)
+            {
+                __result *= compPsyche.Personality.Evaluate(Thought_MoodOffset.MoralityMoodOffsetMultiplier);
+            }
+        }
+    }
+}
