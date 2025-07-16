e8d0b74c88162dcef40f4767a4b4a770c79c62a6
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-07-16 16:03:51 +0900

    Feat: order, thought work

--- Source/1.6/HarmonyPatches/OrderRoomRoleNotification.cs
+++ Source/1.6/HarmonyPatches/OrderRoomRoleNotification.cs
@@ -4,6 +4,15 @@ using Verse;
 
 namespace Maux36.RimPsyche.Disposition
 {
+    [HarmonyPatch(typeof(Bill), "Notify_BillWorkStarted")]
+    public static class Bill_Notify_BillWorkStarted_Patch
+    {
+        static void Postfix(Pawn billDoer)
+        {
+            Log.Message($"{billDoer.Name} started bill");
+        }
+    }
+
     [HarmonyPatch(typeof(Toils_Recipe), "DoRecipeWork")]
     public static class Toils_Recipe_DoRecipeWork_Patch
     {
--- Source/1.6/ThoughtUtil.cs
+++ Source/1.6/ThoughtUtil.cs
@@ -8,8 +8,67 @@ namespace Maux36.RimPsyche.Disposition
     {
         public static Dictionary<string, RimpsycheFormula> MoodMultiplierDB = new()
         {
-            { "KnowPrisonerSold", CompassionMoodMultiplier},
-            { "KnowColonistOrganHarvested", CompassionMoodMultiplier},
+            // Thoughts_Memory_Death
+            { "KnowGuestExecuted", CompassionMoodMultiplier},
+            { "KnowColonistExecuted", CompassionMoodMultiplier},
+            { "KnowPrisonerDiedInnocent", CompassionMoodMultiplier},
+            { "KnowColonistDied", CompassionMoodMultiplier},
+
+            { "BondedAnimalDied", LoyaltyCompassionMoodMultiplier},
+
+            { "PawnWithGoodOpinionDied", SociabilityCompassionMoodMultiplier},
+            { "PawnWithBadOpinionDied", CompassionPositiveMoodMultiplier},
+
+            { "MySonDied", CompassionMoodMultiplier},
+            { "MyDaughterDied", CompassionMoodMultiplier},
+            { "MyHusbandDied", CompassionMoodMultiplier},
+            { "MyWifeDied", CompassionMoodMultiplier},
+            { "MyFianceDied", CompassionMoodMultiplier},
+            { "MyFianceeDied", CompassionMoodMultiplier},
+            { "MyLoverDied", CompassionMoodMultiplier},
+            { "MyBrotherDied", CompassionMoodMultiplier},
+            { "MySisterDied", CompassionMoodMultiplier},
+            { "MyGrandchildDied", CompassionMoodMultiplier},
+            { "MyFatherDied", CompassionMoodMultiplier},
+            { "MyMotherDied", CompassionMoodMultiplier},
+            { "MyNieceDied", CompassionMoodMultiplier},
+            { "MyNephewDied", CompassionMoodMultiplier},
+            { "MyHalfSiblingDied", CompassionMoodMultiplier},
+            { "MyAuntDied", CompassionMoodMultiplier},
+            { "MyUncleDied", CompassionMoodMultiplier},
+            { "MyGrandparentDied", CompassionMoodMultiplier},
+            { "MyCousinDied", CompassionMoodMultiplier},
+            { "MyKinDied", CompassionMoodMultiplier},
+
+            // Thoughts_Memory_Lost
+            { "ColonistLost", CompassionMoodMultiplier},
+
+            { "BondedAnimalReleased", LoyaltyMoodMultiplier},
+            { "BondedAnimalLost", LoyaltyCompassionMoodMultiplier},
+
+            { "PawnWithGoodOpinionLost", SociabilityCompassionMoodMultiplier},
+            { "PawnWithBadOpinionLost", CompassionPositiveMoodMultiplier},
+
+            { "MySonLost", CompassionMoodMultiplier},
+            { "MyDaughterLost", CompassionMoodMultiplier},
+            { "MyHusbandLost", CompassionMoodMultiplier},
+            { "MyWifeLost", CompassionMoodMultiplier},
+            { "MyFianceLost", CompassionMoodMultiplier},
+            { "MyFianceeLost", CompassionMoodMultiplier},
+            { "MyLoverLost", CompassionMoodMultiplier},
+            { "MyBrotherLost", CompassionMoodMultiplier},
+            { "MySisterLost", CompassionMoodMultiplier},
+            { "MyGrandchildLost", CompassionMoodMultiplier},
+            { "MyFatherLost", CompassionMoodMultiplier},
+            { "MyMotherLost", CompassionMoodMultiplier},
+            { "MyNieceLost", CompassionMoodMultiplier},
+            { "MyNephewLost", CompassionMoodMultiplier},
+            { "MyHalfSiblingLost", CompassionMoodMultiplier},
+            { "MyAuntLost", CompassionMoodMultiplier},
+            { "MyUncleLost", CompassionMoodMultiplier},
+            { "MyGrandparentLost", CompassionMoodMultiplier},
+            { "MyCousinLost", CompassionMoodMultiplier},
+            { "MyKinLost", CompassionMoodMultiplier},
         };
 
         public static RimpsycheFormula CompassionMoodMultiplier = new(
@@ -17,8 +76,48 @@ namespace Maux36.RimPsyche.Disposition
             (tracker) =>
             {
                 float mult = 1f;
-                float compassionMult = 1f + tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Compassion)*0.5;
+                float compassionMult = 1f + tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Compassion)*0.5f;
                 return mult * compassionMult;
+            }
+        );
+
+        public static RimpsycheFormula LoyaltyCompassionMoodMultiplier = new(
+            "LoyaltyCompassionMoodMultiplier",
+            (tracker) =>
+            {
+                float mult = 1f;
+                float loyaltyCompassionMult = 1 + tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Compassion)*0.5f + tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Loyalty)*0.5f;
+                return mult * loyaltyCompassionMult;
+            }
+        );
+
+        public static RimpsycheFormula SociabilityCompassionMoodMultiplier = new(
+            "SociabilityCompassionMoodMultiplier",
+            (tracker) =>
+            {
+                float mult = 1f;
+                float loyaltyCompassionMult = 1 + tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Compassion)*0.5f + tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Socialbility)*0.5f;
+                return mult * loyaltyCompassionMult;
+            }
+        );
+
+        public static RimpsycheFormula CompassionPositiveMoodMultiplier = new(
+            "CompassionPositiveMoodMultiplier",
+            (tracker) =>
+            {
+                float mult = 1f;
+                float loyaltyCompassionMult = 1 - tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Compassion)*0.5f;
+                return mult * loyaltyCompassionMult;
+            }
+        );
+
+        public static RimpsycheFormula LoyaltyMoodMultiplier = new(
+            "LoyaltyMoodMultiplier",
+            (tracker) =>
+            {
+                float mult = 1f;
+                float loyaltyCompassionMult = 1 + tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Loyalty)*0.5f;
+                return mult * loyaltyCompassionMult;
             }
         );
     }
