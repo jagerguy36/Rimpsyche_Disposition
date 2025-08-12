3d9b5328a290846ca4d8c30e0b2354c907350f6d
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-08-12 15:59:31 +0900

    Feat: Balance

--- Source/1.6/StatParts/MentalBreakThresholdStatPart.cs
+++ Source/1.6/StatParts/MentalBreakThresholdStatPart.cs
@@ -3,7 +3,7 @@ using Verse;
 
 namespace Maux36.RimPsyche.Disposition
 {
-    public class PsycheVolatilityMentalBreakThreasholdOffset : StatPart// M 0.8 ~ 1.2
+    public class PsycheVolatilityMentalBreakThreasholdOffset : StatPart// M 0.86 ~ 1.14
     {
         public override void TransformValue(StatRequest req, ref float val)
         {
@@ -34,7 +34,7 @@ namespace Maux36.RimPsyche.Disposition
             "TensionMentalBreakThresholdOffset",
             (tracker) =>
             {
-                float resilience = tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Tension) * 0.2f;
+                float resilience = tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Tension) * 0.14f;
                 return resilience;
             }
         );
