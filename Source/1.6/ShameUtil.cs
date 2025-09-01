8585301a40eb00581c02298a45f87909040f10c9
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-09-01 18:54:21 +0900

    Feat: ShameUtil

--- Source/1.6/ShameUtil.cs
+++ Source/1.6/ShameUtil.cs
@@ -8,53 +8,55 @@ namespace Maux36.RimPsyche.Disposition
 {
     public static class ShameUtil
     {
-        //TODO: Check the distance numbers.
+        //Also check TryFindDirectFleeDestination
         public static IntVec3 FindHideInShameLocation(Pawn pawn)
         {
             IntVec3 position = pawn.Position;
-            int bestPosition = -100;
-            IntVec3 cell = pawn.Position;
-            int maxDistance = 40;
-            // First check if the pawn's got a bedroom and that is near enough. If that's the case, hide in their own room.
+            IntVec3 bestCell = position;
+            int bestScore = -100;
+            if (pawn.ownership.OwnedBed != null)
+            {
+                IntVec3 bedPosition = pawn.ownership.OwnedBed.Position;
+                if ((pawn.Position - bedPosition).LengthHorizontalSquared =< 75f)
+                {
+                    return bedPosition
+                }
+            }
+
+            float sightDistance = 13f; //Interaction HorDistance is 6, flee all pawn flee distance is 23
+            float runDistanceMax = 50;
+            float maxDistSquared = 2500;
             FloatRange temperature = pawn.ComfortableTemperatureRange();
             List<Pawn> all_pawns = pawn.Map.mapPawns.AllPawnsSpawned.Where(x
-                => x.Position.DistanceTo(pawn.Position) < 100
+                => x.Position.DistanceTo(pawn.Position) < 63f //sight+rundist
                 && x.RaceProps.Humanlike
                 && x != pawn
                 ).ToList();
 
-            List<IntVec3> random_cells = new List<IntVec3>();
-            for (int loop = 0; loop < 50; ++loop)
+            // Find best candidate
+            for (int i = 0; i < 20; i++)
             {
-                random_cells.Add(position + IntVec3.FromVector3(Vector3Utility.HorizontalVectorFromAngle(Rand.Range(0, 360)) * Rand.RangeInclusive(1, maxDistance)));
-            }
-            random_cells = random_cells.Where(x
-                => x.Standable(pawn.Map)
-                && x.InAllowedArea(pawn)
-                && x.GetDangerFor(pawn, pawn.Map) != Danger.Deadly
-                && !x.ContainsTrap(pawn.Map)
-                && !x.ContainsStaticFire(pawn.Map)
-                ).Distinct().ToList();
+                IntVec3 candidate = position + IntVec3.FromVector3(
+                    Vector3Utility.HorizontalVectorFromAngle(Rand.Range(0, 360)) * Rand.RangeInclusive(1, runDistanceMax));
 
-            foreach (IntVec3 random_cell in random_cells)
-            {
-                if (pawn.Position.DistanceTo(random_cell) > 100)
-                    continue;// too far
+                if (!candidate.InBounds(pawn.Map)) continue;
+                if (!candidate.Standable(pawn.Map)) continue;
+                if (!candidate.InAllowedArea(pawn)) continue;
+                if (candidate.GetDangerFor(pawn, pawn.Map) == Danger.Deadly) continue;
+                if (candidate.ContainsTrap(pawn.Map)) continue;
+                if (candidate.ContainsStaticFire(pawn.Map)) continue;
 
                 int score = 0;
-                Room room = random_cell.GetRoom(pawn.Map);
-
-                bool might_be_seen = MightBeSeen(all_pawns, random_cell, pawn);
+                Room room = candidate.GetRoom(pawn.Map);
+                bool might_be_seen = MightBeSeen(all_pawns, candidate, pawn, sightDistance);
 
                 if (might_be_seen)
-                    score -= 100;
+                    continue;
 
                 if (random_cell.GetTemperature(pawn.Map) > temperature.min && random_cell.GetTemperature(pawn.Map) < temperature.max)
                     score += 20;
                 else
                     score -= 20;
-                if (random_cell.Roofed(pawn.Map))
-                    score += 5;
                 if (random_cell.GetDangerFor(pawn, pawn.Map) == Danger.Some)
                     score -= 25;
                 else if (random_cell.GetDangerFor(pawn, pawn.Map) == Danger.None)
@@ -63,7 +65,6 @@ namespace Maux36.RimPsyche.Disposition
                     random_cell.GetTerrain(pawn.Map) == TerrainDefOf.WaterMovingShallow ||
                     random_cell.GetTerrain(pawn.Map) == TerrainDefOf.WaterOceanShallow)
                     score -= 20;
-
                 if (!room.Owners.Any())
                     score += 10;
                 else if (room.Owners.Contains(pawn))
@@ -72,19 +73,24 @@ namespace Maux36.RimPsyche.Disposition
                     score -= 50;
                 if (room.Role == RoomRoleDefOf.Bedroom)
                     score += 10;
-
-                if (score <= bestPosition) continue;
-                bestPosition = score;
-                cell = random_cell;
+                if (score > bestScore && pawn.CanReach(candidate, PathEndMode.OnCell, Danger.Some))
+                {
+                    bestScore = score;
+                    bestCell = candidate;
+                    if (score >= 50)
+                    {
+                        return bestCell;
+                    }
+                }
             }
-
-            return cell;
+            return bestCell;
         }
-        public static bool MightBeSeen(List<Pawn> otherPawns, IntVec3 cell, Pawn pawn)
+
+        public static bool MightBeSeen(List<Pawn> otherPawns, IntVec3 cell, Pawn pawn, int dist=13)
         {
             return otherPawns.Any(x
                     => x.Awake()
-                    && x.Position.DistanceTo(cell) < 50
+                    && x.Position.DistanceTo(cell) < dist
                     && GenSight.LineOfSight(x.Position, cell, pawn.Map)
                     );
         }
