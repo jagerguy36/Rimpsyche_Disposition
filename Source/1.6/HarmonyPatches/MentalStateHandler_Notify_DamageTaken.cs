30108da7f7d13c53691ac11b21b25166e93091e5
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-09-04 00:28:59 +0900

    Feat: Panic Attack setup

---
+++ Source/1.6/HarmonyPatches/MentalStateHandler_Notify_DamageTaken.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Maux36.RimPsyche.Disposition
+{
+    [HarmonyPatch(typeof(MentalStateHandler), "Notify_DamageTaken")]
+    public static class MentalStateHandler_Notify_DamageTaken
+    {
+        static bool Prefix(Pawn ___pawn, DamageInfo dinfo)
+        {
+            if (___pawn.Faction == Faction.OfPlayer)
+            {
+                if (___pawn.Spawned && ___pawn.MentalStateDef == null && !___pawn.Downed && dinfo.Def.ExternalViolenceFor(___pawn) && ___pawn.RaceProps.Humanlike && ___pawn.mindState.canFleeIndividual)
+                {
+                    float threshold = 0.5f;
+                    float hpp = ___pawn.health.summaryHealth.SummaryHealthPercent;
+                    Log.Message($"{___pawn.Name} took damage| threshold: {threshold} | hpp: {hpp}");
+                    if (true)
+                    {
+                        ___pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.PanicFlee, "cowardFlee".Translate(), forced: false, forceWake: false, causedByMood: false, null, transitionSilently: false, causedByDamage: true);
+                    }
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/1.6/MentalState/PanicAttack.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse.AI;
+
+namespace Maux36.RimPsyche.Disposition
+{
+    public class PanicAttack : MentalState
+    {
+        public override RandomSocialMode SocialModeMax()
+        {
+            return RandomSocialMode.Off;
+        }
+    }
+}
