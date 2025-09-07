f85693931e4207312eeb151fd4c954962e843838
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-09-07 12:49:25 +0900

    Fix: dirty code

--- Source/1.6/JoyGivers/JoyGiver_Meditate_Reflectiveness.cs
+++ Source/1.6/JoyGivers/JoyGiver_Meditate_Reflectiveness.cs
@@ -1,33 +1,33 @@
 using RimWorld;
 using Verse;
-using Verse.AI;
 
-namespace Maux36.RimPsyche.Disposition;
-
-public class JoyGiver_Meditate_Reflectiveness : JoyGiver_Meditate
+namespace Maux36.RimPsyche.Disposition
 {
-	public override float GetChance(Pawn pawn)
-	{
-		var compPsyche = pawn.compPsyche();
-		if(compPsyche.Enabled != true)
-		{
-			return def.baseChance;
-		}
-		else
-		{
-			return def.baseChance*compPsyche.Evaluate(MeditateChanceMultiplier);;
-		}
-	}
+    public class JoyGiver_Meditate_Reflectiveness : JoyGiver_Meditate
+    {
+        public override float GetChance(Pawn pawn)
+        {
+            var compPsyche = pawn.compPsyche();
+            if (compPsyche.Enabled != true)
+            {
+                return def.baseChance;
+            }
+            else
+            {
+                return def.baseChance * compPsyche.Evaluate(MeditateChanceMultiplier);
+            }
+        }
 
-	public static RimpsycheFormula MeditateChanceMultiplier = new(
-		"MeditateChanceMultiplier",
-		(tracker) =>
-		{
-			float r = tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Reflectiveness);
-			float c = -2.5f * r * (r - 2f);
-			if (c > 0) return c;
-			return 0f;
-		},
-            RimpsycheFormulaManager.FormulaIdDict
-    );
+        public static RimpsycheFormula MeditateChanceMultiplier = new(
+            "MeditateChanceMultiplier",
+            (tracker) =>
+            {
+                float r = tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Reflectiveness);
+                float c = -2.5f * r * (r - 2f);
+                if (c > 0) return c;
+                return 0f;
+            },
+                RimpsycheFormulaManager.FormulaIdDict
+        );
+    }
 }
