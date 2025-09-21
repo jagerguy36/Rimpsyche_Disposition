4db9d26043e08b953571d75003f66dd696cf8cd5
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-09-21 21:56:06 +0900

    Fix: locale work base

--- Source/1.6/StatParts/Deliberation/PsycheDeliberationButcherEfficiency.cs
+++ Source/1.6/StatParts/Deliberation/PsycheDeliberationButcherEfficiency.cs
@@ -24,7 +24,7 @@ namespace Maux36.RimPsyche.Disposition
                 var compPsyche = pawn.compPsyche();
                 if (compPsyche?.Enabled == true)
                 {
-                    return "RP_Stat_Psyche".Translate() + "\n    " + "DeliberationButcherEfficiency".Translate() + ": x" + compPsyche.Evaluate(DeliberationButcherEfficiencyyMultiplier).ToStringPercent()+"\n";
+                    return "RP_Stat_Psyche".Translate() + "\n    " + "RP_Stat_DeliberationButcherEfficiency".Translate() + ": x" + compPsyche.Evaluate(DeliberationButcherEfficiencyyMultiplier).ToStringPercent()+"\n";
                 }
             }
             return null;
