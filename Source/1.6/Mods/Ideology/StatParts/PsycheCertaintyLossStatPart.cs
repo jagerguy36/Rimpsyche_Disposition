e53763a4bd020b01661feed9cf146eb127172ce2
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-10-17 17:46:18 +0900

    Fix: Wrong number

--- Source/1.6/Mods/Ideology/StatParts/PsycheCertaintyLossStatPart.cs
+++ Source/1.6/Mods/Ideology/StatParts/PsycheCertaintyLossStatPart.cs
@@ -37,8 +37,8 @@ namespace Maux36.RimPsyche.Disposition.Ideology
                 float confidence = tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Confidence);
                 float openness = tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Openness);
                 float trust = tracker.GetPersonality(PersonalityDefOf.Rimpsyche_Trust);
-                float conviction = (confidence - openness) * 0.5f;
-                float shake = trust - conviction;
+                float conviction = (confidence - openness) * 0.5f; // -1~1
+                float shake = (trust - conviction) * 0.5f; // -1~1
                 if (shake >= 0f)
                 {
                     return 0.5f * shake + 1f;
