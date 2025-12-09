9b65b0a419d1660981af6b84faf413778eec13b5
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-09 17:40:25 +0900

    Feat: tmp joychance mult

--- Source/1.6/Utils/ThoughtUtil.cs
+++ Source/1.6/Utils/ThoughtUtil.cs
@@ -100,7 +100,102 @@ namespace Maux36.RimPsyche.Disposition
             if (ModsConfig.OdysseyActive) OdysseyDB.AddDefs_Odyssey(MoodThoughtTagDB, OpinionThoughtTagDB);
             MiscModDB.AddDefs_MiscMods(MoodThoughtTagDB, OpinionThoughtTagDB);
         }
+    }
 
-        
+    //TODO: move to proper place, modularize formula registry
+    //JoyGiverUtil.cs
+    [StaticConstructorOnStartup]
+    public static class JoyGiverUtil
+    {
+        private static readonly bool useIndividualJoychanceSetting = RimpsycheDispositionSettings.useIndividualJoychance;
+        private static readonly float JoyChanceCurveC = RimpsycheDispositionSettings.joychanceIndividualC;
+        public static Dictionary<int, RimpsycheFormula> JoyChanceDB = [];
+
+        static JoyGiverUtil()
+        {
+            Initialize();
+        }
+
+        public static void Initialize()
+        {
+            AddBaseJoyGivers();
+            ModCompat();
+        }
+        private static void AddBaseJoyGivers()
+        {
+            JoyGiverDB.AddDefs_Vanilla(JoyChanceDB);
+        }
+        public static void ModCompat()
+        {
+            JoyGiverDB.AddDefs_Mods(JoyChanceDB);
+        }
+
+        public static float JoychanceCurve(float mood)
+        {
+            if (mood >= 0)
+            {
+                return 1f + (JoyChanceCurveC - (JoyChanceCurveC / ((2f * mood) + 1f)));
+            }
+            else
+            {
+                //Needs tweak
+                return 1f + ((JoyChanceCurveC / (1f - (2f * mood))) - JoyChanceCurveC);
+            }
+        }
+    }
+
+    //JoyGiverDB.cs
+
+    public class JoyGiverDB
+    {
+        public static void AddDefs_Vanilla(Dictionary<int, RimpsycheFormula> JoyChanceDB)
+        {
+            // RegisterJoyChanceMultiplier("JoyGiver_Meditate", JoyChanceDB, MeditateChanceMultiplier)
+        }
+        public static void AddDefs_Mods(Dictionary<int, RimpsycheFormula> JoyChanceDB)
+        {
+            if (ModsConfig.IsActive("dubwise.dubsbadhygiene"))
+            {
+                // RegisterJoyChanceMultiplier("WatchWashingMachine", JoyChanceDB, MeditateChanceMultiplier)
+            }
+        }
+
+
+        public static void RegisterJoyChanceMultiplier(string defName, Dictionary<int, RimpsycheFormula> targetDb, RimpsycheFormula value)
+        {
+            var thoughtDef = DefDatabase<JoyGiverDef>.GetNamed(defName, false);
+            if (thoughtDef != null)
+            {
+                targetDb[thoughtDef.shortHash] = value;
+            }
+            else
+            {
+                Log.Warning($"[Rimpsyche] Could not find JoyGiverDef named '{defName}'.");
+            }
+        }
+    }
+
+    //Harmony
+    //TODO: move to proper folder
+    [HarmonyPatch(typeof(JoyGiver), "GetChance")]
+    public static class JoyGiver_GetChance_Patch
+    {
+        public static void Postfix(ref float __result, JoyGiver __instance, Pawn pawn)
+        {
+            if (__result == 0f || pawn == null)
+                return;
+            var compPsyche = pawn.compPsyche();
+            if (compPsyche == null || !compPsyche.Enabled)
+                return;
+
+            //TODO: implement joychance cache
+            if (JoyChanceDB.TryGetValue(__instance.def.shorthash,  out RimpsycheFormula joychanceFormula))
+            {
+                if (joychanceFormula != null)
+                {
+                    __result *= compPsyche.Evaluate(thoughtFormula);
+                }
+            }
+        }
     }
 }
