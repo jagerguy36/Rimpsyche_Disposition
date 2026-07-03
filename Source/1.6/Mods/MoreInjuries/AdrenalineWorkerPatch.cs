8c7539abae5237d41e04324fb78d42ad93ab828b
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2026-07-03 18:18:22 +0900

    Feat: More injuries integration

---
+++ Source/1.6/Mods/MoreInjuries/AdrenalineWorkerPatch.cs
@@ -0,0 +1,84 @@
+﻿using HarmonyLib;
+using MoreInjuries;
+using MoreInjuries.Defs.WellKnown;
+using System;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Maux36.RimPsyche.Disposition.MoreInjuries
+{
+    [HarmonyPatch]
+    public static class AdrenalineWorkerPatch
+    {
+        public static bool Prepare()
+        {
+            if (RimpsycheDispositionSettings.useFightorFlight)
+                return true;
+            return false;
+        }
+
+        static MethodBase TargetMethod()
+        {
+            var type = AccessTools.TypeByName(
+                "MoreInjuries.HealthConditions.AdrenalineRush.AdrenalineWorker");
+            return AccessTools.PropertyGetter(type, "IsEnabled");
+        }
+
+        static bool Prefix(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(FightorFlightUtil), nameof(FightorFlightUtil.NotifyDamageTaken))]
+    public static class NotifyDamageTakenPatch
+    {
+        static bool Prefix(Pawn pawn, float damage, float psycheAdrenalineValue)
+        {
+            if (damage <= 0f) return false;
+            if (MoreInjuriesMod.Settings.EnableAdrenaline)
+            {
+                if (psycheAdrenalineValue > 0f)
+                {
+                    if (!pawn.health.hediffSet.TryGetHediff(KnownHediffDefOf.AdrenalineRush, out Hediff adrenalineRush))
+                    {
+                        adrenalineRush = HediffMaker.MakeHediff(KnownHediffDefOf.AdrenalineRush, pawn);
+                        adrenalineRush.Severity = 0;
+                        pawn.health.AddHediff(adrenalineRush);
+                    }
+                    float dmgPercent = damage / pawn.health.LethalDamageThreshold;
+                    float severity = psycheAdrenalineValue * dmgPercent;
+                    float newSeverity = adrenalineRush.Severity + severity;
+                    adrenalineRush.Severity = Mathf.Min(newSeverity, 1.8f);
+                }
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(FightorFlightUtil), nameof(FightorFlightUtil.ApplyAdrenaline))]
+    public static class ApplyAdrenalinePatch
+    {
+        static bool Prefix(Pawn pawn, float gain)
+        {
+            if (gain <= 0) return false;
+            if (MoreInjuriesMod.Settings.EnableAdrenaline)
+            {
+                if (!pawn.health.hediffSet.TryGetHediff(KnownHediffDefOf.AdrenalineRush, out Hediff adrenalineRush))
+                {
+                    adrenalineRush = HediffMaker.MakeHediff(KnownHediffDefOf.AdrenalineRush, pawn);
+                    adrenalineRush.Severity = 0;
+                    pawn.health.AddHediff(adrenalineRush);
+                }
+                float newSeverity = adrenalineRush.Severity + gain;
+                adrenalineRush.Severity = Mathf.Min(newSeverity, 1f);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/1.6/Mods/MoreInjuries/HarmonyInit.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using Verse;
+
+namespace Maux36.RimPsyche.Disposition.MoreInjuries
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyInit
+    {
+        static HarmonyInit()
+        {
+            var harmony = new Harmony("rimworld.mod.Maux.RimPsyche.Disposition.MoreInjuries");
+            try
+            {
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+                Log.Message("[Rimpsyche - Disposition] Patched More Injuries");
+            }
+            catch (Exception e)
+            {
+                Log.Error($"[Rimpsyche - Disposition] More Injuries patch failed: {e}");
+            }
+        }
+    }
+}
